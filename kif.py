dea1e4bf1dea726e647d7c52288439b62689ceef
sicarie <sicarie@gmail.com>
2015-09-27 12:10:32 -0700

    Commented rough direction of kif.py

--- kif.py
+++ kif.py
@@ -8,13 +8,33 @@ project.
 import logging
 import os
 
+
 #import lib.defaults as default
+import lib.parse_service_configs.py as parse_service_configs
+
 
 def main():
     _init_logger()
     logger.info('Starting program.')
 
-    _check_root()
+    _check_root()  # I don't think we care if we're root, but it's here just in case
+    
+    # Try non-intrusive recon first, so check if we can get into service logs
+    services = parse_service_configs.get_service_list()
+    for service in services:
+        # log attempted load
+        # load service yaml
+        # if successful, log config details to kif.log
+        pass
+    
+    # Is there a way to determine OpenStack release (juno, havana, kilo, etc...)?
+    
+    # Probably going to use requests lib for REST query
+    # Determine API endpoints
+    # Get token
+    # Source openrc
+    # Enumerate attacks in modules/
+    # Call with populated options
 
 
 def _init_logger():
