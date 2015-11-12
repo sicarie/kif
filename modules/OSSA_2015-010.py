4d6c5c8516343e87e2cc0511d84ee7d188977dcf
sicarie <nathaniel.dillon@hp.com>
2015-11-12 14:56:07 -0800

    Adding heat template for OSSA 2015-010

---
+++ modules/OSSA_2015-010.py
@@ -0,0 +1,9 @@
+description: Does not matter
+heat_template_version: '2013-05-23'
+outputs: {}
+parameters:
+  param1:
+    type: string
+    label: normal_label
+    description: hack="><script>alert('YOUR HORIZON IS PWNED')</script>"
+resources: {}
